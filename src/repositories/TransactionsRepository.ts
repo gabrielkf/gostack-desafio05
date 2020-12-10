ac369a5ee5cec9ff8bc075105877cc0bbe33a66a
gabrielkf <gabrielkf@gmail.com>
2020-12-09 22:18:01 -0300

    Implemented requested routes with SoC

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,6 +1,11 @@
 import Transaction from '../models/Transaction';
 
-interface Balance {
+interface Input {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+export interface Balance {
   income: number;
   outcome: number;
   total: number;
@@ -14,15 +19,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+
+    this.transactions.forEach(transaction => {
+      switch (transaction.type) {
+        case 'income':
+          income += transaction.value;
+          break;
+        case 'outcome':
+          outcome += transaction.value;
+          break;
+        default:
+          throw Error(`Unexpected type of transaction ${transaction.type}`);
+      }
+    });
+
+    return { income, outcome, total: income - outcome };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Input): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
